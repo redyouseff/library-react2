644257eca3410bf9e032a3745e36574960778900
redyousef <120760435+redyouseff@users.noreply.github.com>
2024-07-22 14:47:12 +0300

    v2

--- src/api/BaseUrl.js
+++ src/api/BaseUrl.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-const BaseUrl=axios.create({baseURL:"http://localhost:1000"})
+const BaseUrl=axios.create({baseURL:"https://gcm-6lio.onrender.com"})
 export default BaseUrl;
